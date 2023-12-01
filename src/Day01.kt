90ac8ce7ca4d9d4f6b0609421c677e0b7f2741a8
Valentin Scherer <schererv@student.ethz.ch>
2023-12-01 19:32:38 +0100

    Solution Day1_1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,11 @@
 fun main() {
+    fun extractCalibrationValue(s: String) : Int {
+        val digits = s.filter { it.isDigit() }
+        return "${digits.first()}${digits.last()}".toInt()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { s -> extractCalibrationValue(s) }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +14,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
+//    println(part1(testInput))
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
