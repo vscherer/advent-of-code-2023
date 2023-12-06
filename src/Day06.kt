32a0c13caaa8a175a7f6e0606f5676f988794b2e
Valentin Scherer <15944221+vscherer@users.noreply.github.com>
2023-12-06 10:00:22 +0100

    Solution Day6

---
+++ src/Day06.kt
@@ -0,0 +1,10 @@
+/**
+ * Done by hand with help of a big number calculator.
+ *
+ * Equalise time at
+ * x = (t +- sqrt(t^2-4*d)) / 2
+ *
+ * Round lower result up and higher result down.
+ *
+ * Number of possible ways per race is = | x1 - x2 | + 1
+ */
