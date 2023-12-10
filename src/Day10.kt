4be22a049c1c61ce41b2522a17f60b11164c33ac
Valentin Scherer <15944221+vscherer@users.noreply.github.com>
2023-12-10 14:11:07 +0100

    Solution Day10_2

--- src/Day10.kt
+++ src/Day10.kt
@@ -2,9 +2,12 @@ import Direction.*
 
 private const val DAY = "10"
 private const val SOLUTION_TEST_1 = 8
-private const val SOLUTION_TEST_2 = 0
+private const val SOLUTION_TEST_2 = 10
 
 private typealias Grid = List<String>
+private typealias MutableGrid = MutableList<String>
+
+private fun Grid.toMutableGrid(): MutableGrid = this.toMutableList()
 
 private enum class Direction {
     NORTH,
@@ -23,15 +26,20 @@ private fun Pair<Int, Int>.moveOne(dir: Direction): Pair<Int, Int> {
 }
 
 private fun Grid.get(coordinates: Pair<Int, Int>) = this[coordinates.first][coordinates.second]
+private fun MutableGrid.set(symbol: Char, coordinates: Pair<Int, Int>) {
+    val sb = StringBuilder(this[coordinates.first])
+    sb.setCharAt(coordinates.second, symbol)
+    this[coordinates.first] = sb.toString()
+}
 
-private fun Grid.pad(): Grid {
+private fun Grid.padWith(paddingChar: Char): Grid {
     val paddedSides = this
         .map { row ->
             row
-                .padStart(row.length + 1, '.')
-                .padEnd(row.length + 2, '.')
+                .padStart(row.length + 1, paddingChar)
+                .padEnd(row.length + 2, paddingChar)
         }
-    val emptyRow = ".".repeat(paddedSides[0].length)
+    val emptyRow = "$paddingChar".repeat(paddedSides[0].length)
 
     return buildList {
         add(emptyRow)
@@ -49,6 +57,15 @@ private fun findStart(grid: Grid): Pair<Int, Int> {
     throw IllegalStateException("No Start in input")
 }
 
+private fun findStartDirection(grid: Grid, start: Pair<Int, Int>): Direction {
+    return when {
+        listOf('|', '7', 'F').contains(grid.get(start.moveOne(NORTH))) -> NORTH
+        listOf('|', 'J', 'L').contains(grid.get(start.moveOne(SOUTH))) -> SOUTH
+        listOf('-', '7', 'J').contains(grid.get(start.moveOne(EAST))) -> EAST
+        else -> WEST
+    }
+}
+
 /**
  * Assumes previous connection is valid
  */
@@ -64,45 +81,195 @@ private fun findNextDirection(symbol: Char, direction: Direction): Direction {
     }
 }
 
-private fun followPath(grid: Grid, start: Pair<Int, Int>): Int {
-    grid.forEach(::println)
-    var currentDirection = when {
-        listOf('|', '7', 'F').contains(grid.get(start.moveOne(NORTH))) -> NORTH
-        listOf('|', 'J', 'L').contains(grid.get(start.moveOne(SOUTH))) -> SOUTH
-        listOf('-', '7', 'J').contains(grid.get(start.moveOne(EAST))) -> EAST
-        else -> WEST
-    }
+private fun countPath(grid: Grid, start: Pair<Int, Int>): Int {
+    var currentDirection = findStartDirection(grid, start)
     var currentPosition = start.moveOne(currentDirection)
-    println("Moving $currentDirection to $currentPosition")
     var steps = 1
 
     while (grid.get(currentPosition) != 'S') {
         currentDirection = findNextDirection(grid.get(currentPosition), currentDirection)
         currentPosition = currentPosition.moveOne(currentDirection)
         steps++
-        println("Moving $currentDirection to $currentPosition")
     }
 
     return steps
 }
 
+
+private fun findStartSymbol(`in`: Direction, out: Direction): Char {
+    return when (`in`) {
+        NORTH -> {
+            when (out) {
+                NORTH -> '|'
+                EAST -> 'F'
+                WEST -> '7'
+                else -> throw IllegalStateException()
+            }
+        }
+
+        SOUTH -> {
+            when (out) {
+                SOUTH -> '|'
+                EAST -> 'L'
+                WEST -> 'J'
+                else -> throw IllegalStateException()
+            }
+        }
+
+        EAST -> {
+            when (out) {
+                NORTH -> 'J'
+                SOUTH -> '7'
+                EAST -> '-'
+                else -> throw IllegalStateException()
+            }
+        }
+
+        else -> { // WEST
+            when (out) {
+                NORTH -> 'L'
+                SOUTH -> 'F'
+                WEST -> '-'
+                else -> throw IllegalStateException()
+            }
+        }
+    }
+}
+
+private fun paintPath(grid: Grid, start: Pair<Int, Int>): Grid {
+    val gridCopy = MutableList(grid.size) { ".".repeat(grid[0].length) }
+
+    val firstDirection = findStartDirection(grid, start)
+    var currentDirection = firstDirection
+    var currentPosition = start.moveOne(currentDirection)
+
+    while (grid.get(currentPosition) != 'S') {
+        val currentSymbol = grid.get(currentPosition)
+        gridCopy.set(currentSymbol, currentPosition)
+
+        currentDirection = findNextDirection(grid.get(currentPosition), currentDirection)
+        currentPosition = currentPosition.moveOne(currentDirection)
+    }
+
+    gridCopy.set(findStartSymbol(currentDirection, firstDirection), start)
+
+    return gridCopy
+}
+
+private fun Grid.zoomByThree(): Grid {
+    return this.flatMap { row ->
+        val rowAbove = row
+            .replace('|', 'X')
+            .replace('L', 'X')
+            .replace('J', 'X')
+            .map { if (it != 'X') ',' else it }
+            .joinToString("") { ",$it," }
+
+        val rowBelow = row
+            .replace('|', 'X')
+            .replace('F', 'X')
+            .replace('7', 'X')
+            .map { if (it != 'X') ',' else it }
+            .joinToString("") { ",$it," }
+
+        val theRow = row
+            .replace("L", ",XX")
+            .replace("J", "XX,")
+            .replace("F", ",XX")
+            .replace("7", "XX,")
+            .replace("|", ",X,")
+            .replace("-", "XXX")
+            .replace(".", ",,,")
+
+        check(rowAbove.length == rowBelow.length && theRow.length == rowAbove.length)
+        listOf(rowAbove, theRow, rowBelow)
+    }
+}
+
+private fun Grid.shrinkByThree(): Grid {
+    return this
+        .chunked(3)
+        .map { it[1] }
+        .map { row ->
+            row
+                .toCharArray()
+                .toList()
+                .chunked(3)
+                .map { it[1] }
+                .joinToString("")
+        }
+}
+
+private fun dilute(grid: Grid): Grid {
+    return grid.mapIndexed { rowIndex, row ->
+        row
+            .replace("O,", "OO")
+            .replace(",O", "OO")
+            .mapIndexed { colIndex, symbol ->
+                if (symbol == ',') {
+                    if (
+                        (rowIndex > 0 && grid.get(Pair(rowIndex - 1, colIndex)) == 'O')
+                        || (rowIndex < grid.size - 1 && grid.get(Pair(rowIndex + 1, colIndex)) == 'O')
+                    ) 'O' else symbol
+                } else {
+                    symbol
+                }
+            }
+            .joinToString("")
+    }
+}
+
 private fun part1(input: List<String>): Int {
-    val paddedGrid = input.pad()
+    val paddedGrid = input.padWith('.')
+    paddedGrid.forEach(::println)
     val start = findStart(paddedGrid)
     println("Start: $start")
-    return followPath(paddedGrid, start) / 2
+    return countPath(paddedGrid, start) / 2
 }
 
 private fun part2(input: List<String>): Int {
-    return 0
+    val paddedGrid = input.padWith('.')
+    println("\nInput grid:")
+    paddedGrid.forEach(::println)
+
+    val start = findStart(paddedGrid)
+    println("Start: $start")
+
+    val loopOnlyGrid = paintPath(paddedGrid, start)
+    println("\nOnly loop grid:")
+    loopOnlyGrid.forEach(::println)
+
+    val zoomedGrid = loopOnlyGrid.zoomByThree()
+    println("\nZoomed grid:")
+    zoomedGrid.forEach(::println)
+
+    var currentGrid = zoomedGrid.padWith('O').padWith('O').padWith('O')
+    var dilutedGrid = dilute(currentGrid)
+
+    while (dilutedGrid != currentGrid) {
+        currentGrid = dilutedGrid
+        dilutedGrid = dilute(currentGrid)
+    }
+    println("\nMarked grid:")
+    currentGrid.forEach(::println)
+
+    val shrunkGrid = currentGrid.shrinkByThree()
+    println("\nShrunk grid:")
+    shrunkGrid.forEach(::println)
+
+    return shrunkGrid.joinToString("").count { it == ',' }
 }
 
 fun main() {
+    println("\n PART 1 TEST\n")
     testPart1()
+    println("\n PART 1 \n")
     runPart1()
 
-//    testPart2()
-//    runPart2()
+    println("\n PART 2 TEST\n")
+    testPart2()
+    println("\n PART 2 \n")
+    runPart2()
 }
 
 /**
